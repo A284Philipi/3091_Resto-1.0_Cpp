b15c25fb70aa6b0055a9c31f198ff2272a67b698
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 21:36:24 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    long long int a, b, x;
+    cin >> a;
+    cin >> b;
+    x = a % b;
+    cout << x <<endl;
+    return 0;
+}
